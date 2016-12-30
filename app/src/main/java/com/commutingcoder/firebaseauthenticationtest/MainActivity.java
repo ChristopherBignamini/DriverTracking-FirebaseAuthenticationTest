20995cfb5d8748f28516e0c7b9728461fe244fd2
Christopher Bignamini <christopher.bignamini@gmail.com>
2016-12-30 13:53:13 +0100

    Added code for user connection status checking. Still under dev but this is a possible solution for issue 5. Moved contact-user matching code to another activity

--- app/src/main/java/com/commutingcoder/firebaseauthenticationtest/MainActivity.java
+++ app/src/main/java/com/commutingcoder/firebaseauthenticationtest/MainActivity.java
@@ -1,13 +1,8 @@
 package com.commutingcoder.firebaseauthenticationtest;
 
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.database.Cursor;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -27,11 +22,8 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
+import com.google.firebase.database.OnDisconnect;
 import com.google.firebase.database.ValueEventListener;
-
-import java.util.ArrayList;
-import java.util.List;
 
 // TODO: online-offline status in user data-db: what about a singleton like in CriminalIntent
 // TODO: use a local sqlite db for current list of available contacts??
@@ -39,14 +31,14 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private final String TAG = "MainActivity";
-    final static int MY_PERMISSIONS_READ_CONTACTS = 0;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
     private DatabaseReference mUsersReference;
+    private DatabaseReference mConnectionReference;
+
 
     private EditText mEmailEditText;
     private EditText mPhoneEditText;
-    private EditText mNameEditText;
     private EditText mPasswordEditText;
     private Button mSignUpButton;
     private Button mSignInButton;
@@ -55,11 +47,11 @@ public class MainActivity extends AppCompatActivity {
     private Button mCheckStatusButton;
     private TextView mUserStatusText;
     private Button mChooseContactButton;
-    private TextView mContactChosenText;
     private boolean mIsEmailValid;
     private boolean mIsPhoneValid;
     private boolean mIsPasswordValid;
-    private boolean mIsUserLoggedIn;
+    private boolean mIsUserSignedIn;
+    private boolean mIsUserConnected;
     private String mFireBaseUid;
 
     @Override
@@ -75,19 +67,14 @@ public class MainActivity extends AppCompatActivity {
                 FirebaseUser user = firebaseAuth.getCurrentUser();
                 if (user != null) {
                     // User is signed in
-                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                    Log.i(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
                 } else {
                     // User is signed out
-                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                    Log.i(TAG, "onAuthStateChanged:signed_out");
                 }
                 // ...
             }
         };
-
-
-        // Setup reference to rt database
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        mUsersReference = database.getReference("users");
 
         // Users data setup
         // TODO: add SharedPreferences retrieval
@@ -106,7 +93,6 @@ public class MainActivity extends AppCompatActivity {
         mCheckStatusButton = (Button) findViewById(R.id.check_status_button);
         mUserStatusText = (TextView) findViewById(R.id.user_status_text);
         mChooseContactButton = (Button) findViewById(R.id.choose_contact_button);
-        mContactChosenText = (TextView) findViewById(R.id.chosen_contact_text);
 
 
         // UI elements setup
@@ -114,9 +100,6 @@ public class MainActivity extends AppCompatActivity {
         mSignUpButton.setEnabled(false);
         mSignInButton.setEnabled(false);
         mChooseContactButton.setEnabled(false);
-
-        // Check current user status
-        checkStatus();
 
         // User data insert
         mEmailEditText.addTextChangedListener(new TextWatcher() {
@@ -206,13 +189,14 @@ public class MainActivity extends AppCompatActivity {
         mSignOutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(mIsUserLoggedIn) {
+                if(mIsUserSignedIn) {
                     mAuth.signOut();
                     Toast.makeText(getApplicationContext(),"Signed out",Toast.LENGTH_LONG).show();
                 } else {
-                    Toast.makeText(getApplicationContext(),"Already not logged int!",Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(),"Already not signed in!",Toast.LENGTH_LONG).show();
                 }
-                checkStatus();
+                checkAuthenticationStatus();
+                checkConnectionStatus();
             }
         });
         mClearButton.setOnClickListener(new View.OnClickListener() {
@@ -227,7 +211,8 @@ public class MainActivity extends AppCompatActivity {
         mCheckStatusButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                checkStatus();
+                checkAuthenticationStatus();
+                checkConnectionStatus();
             }
         });
 
@@ -239,112 +224,40 @@ public class MainActivity extends AppCompatActivity {
                 // TODO: move in right place
                 Intent intent = new Intent(MainActivity.this, ChooseContactActivity.class);
                 startActivity(intent);
-
-                // Access list of app users
-                Query userQuery = mUsersReference.orderByKey();
-                userQuery.addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-
-                        // TODO: very bad piece of code
-                        List<String> appUsersPhones = new ArrayList<String>();
-                        List<Boolean> appUsersStatus = new ArrayList<Boolean>();
-                        for (DataSnapshot singleSnapshot : dataSnapshot.getChildren()) {
-                            appUsersPhones.add(singleSnapshot.child("phone").getValue().toString());
-                            appUsersStatus.add((Boolean) singleSnapshot.child("status").getValue());
-                        }
-
-                        // TODO: check for best way to handle this, particularly first run aftewe install fails!!
-                        // Here, thisActivity is the current activity
-                        // TODO: use new requestPermissions
-                        if (ContextCompat.checkSelfPermission(getBaseContext(),
-                                android.Manifest.permission.READ_CONTACTS)
-                                != PackageManager.PERMISSION_GRANTED) {
-
-                            // Should we show an explanation?
-                            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,// TODO: I'm not sure about MainActivity.this
-                                    android.Manifest.permission.READ_CONTACTS)) {
-
-                                // Show an expanation to the user *asynchronously* -- don't block
-                                // this thread waiting for the user's response! After the user
-                                // sees the explanation, try again to request the permission.
-
-                            } else {
-
-                                // No explanation needed, we can request the permission.
-
-                                ActivityCompat.requestPermissions(MainActivity.this,// TODO: I'm not sure about MainActivity.this
-                                        new String[]{android.Manifest.permission.READ_CONTACTS},
-                                        MY_PERMISSIONS_READ_CONTACTS);
-
-                                // MY_PERMISSIONS_READ_CONTACTS is an
-                                // app-defined int constant. The callback method gets the
-                                // result of the request.
-                            }
-                        }
-
-                        String[] queryFields = new String[] {
-                                ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
-                                ContactsContract.CommonDataKinds.Phone.NUMBER};
-
-                        Cursor cursor = MainActivity.this.getContentResolver()// TODO: I'm not sure about MainActivity.this
-                                .query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-                                        queryFields,null,null,null);
-
-                        // TODO: avoid multiple delete
-                        Users users = Users.get();// TODO: add save/retrieval code accross lifetime steps (Local db?)
-                        users.deleteAll();
-                        // TODO: find best way to do this search and avoid multiple search
-                        for(int userIndex=0;userIndex<appUsersPhones.size();++userIndex) {
-                            final String currentUserPhone = new String(appUsersPhones.get(userIndex));
-                            Log.d(TAG,"out");
-                            for (cursor.moveToFirst();!cursor.isAfterLast();cursor.moveToNext()) {
-                                Log.d(TAG,"in");
-                                Log.d(TAG,"cursor.getString(1) " + cursor.getString(1));
-                                if(cursor.getString(1).equals(currentUserPhone)) {
-                                    users.addUserData(new UserData(cursor.getString(1),
-                                        cursor.getString(0), appUsersStatus.get(userIndex)));
-                                }
-                            }
-                        }
-
-                        // TODO: Debug only
-                        for (int userIndex=0;userIndex<users.getNumberUsers();++userIndex) {
-                            Log.d(TAG,"Contact number: " + userIndex +
-                                    " name: " + users.getUserData(userIndex).getName() +
-                                    " phone: " + users.getUserData(userIndex).getPhoneNumber() +
-                                    " status: " + users.getUserData(userIndex).getStatus());
-                        }
-
-
-                    }
-
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
-
-                    }
-                });
-
-
-
-
             }
         });
 
+        // TODO: find final place for this call
+        checkAuthenticationStatus();
+        checkConnectionStatus();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         mAuth.addAuthStateListener(mAuthListener);
+
+        // Check current user status
+        checkAuthenticationStatus();
+        checkConnectionStatus();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
+        Log.i(TAG,"onStop");
         if (mAuthListener != null) {
             mAuth.removeAuthStateListener(mAuthListener);
         }
+
+        // Set the user as not signedIn // TODO: temporary solution, use IntentService or similar
+        mIsUserSignedIn = false;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG,"onDestroy");
     }
 
     // TODO: input pars not needed, can be obtained from data member
@@ -358,7 +271,7 @@ public class MainActivity extends AppCompatActivity {
                     // If sign in fails, display a message to the user. If sign in succeeds
                     // the auth state listener will be notified and logic to handle the
                     // signed in user can be handled in the listener.
-                    Log.d(TAG, "createUserWithEmail:onComplete:" + task.getException());
+                    Log.i(TAG, "createUserWithEmail:onComplete:" + task.getException());
                     if (!task.isSuccessful()) {
                         Toast.makeText(getApplicationContext(),
                                 "Account creation error: " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
@@ -376,9 +289,10 @@ public class MainActivity extends AppCompatActivity {
                     }
 
 
-                    // TODO: is the creation task asynchronous? In this case checkStatus must be here
+                    // TODO: is the creation task asynchronous? In this case checkAuthenticationStatus must be here
                     // Check current user status
-                    checkStatus();
+                    checkAuthenticationStatus();
+                    checkConnectionStatus();
 
                 }
             });
@@ -393,7 +307,7 @@ public class MainActivity extends AppCompatActivity {
                     // If sign in fails, display a message to the user. If sign in succeeds
                     // the auth state listener will be notified and logic to handle the
                     // signed in user can be handled in the listener.
-                    Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+                    Log.i(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
                     if (!task.isSuccessful()) {
                         Toast.makeText(getApplicationContext(),
                                 "Sign in failed: " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
@@ -404,17 +318,18 @@ public class MainActivity extends AppCompatActivity {
 
                     // ...
 
-                    // TODO: is the creation task asynchronous? In this case checkStatus must be here
+                    // TODO: is the creation task asynchronous? In this case checkAuthenticationStatus must be here
                     // Check current user status
-                    checkStatus();
+                    checkAuthenticationStatus();
+                    checkConnectionStatus();
 
                 }
             });
     }
 
 
-    private void checkStatus() {
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser(); // TODO: bug, questo funziona anche senza connessione..
+    private void checkAuthenticationStatus() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         if (user != null) {
             String email = user.getEmail();
 
@@ -422,18 +337,60 @@ public class MainActivity extends AppCompatActivity {
             // authenticate with your backend server, if you have one. Use
             // FirebaseUser.getToken() instead.
             mFireBaseUid = user.getUid(); // TODO: this assignement is already done in createAccount
-            Log.d(TAG, "User logged in with email: " + email);
-            mUserStatusText.setText("Logged in with email " + email);
-            mIsUserLoggedIn = true;
-            mChooseContactButton.setEnabled(true);
+            Log.i(TAG, "User signed in with email: " + email);
+            mUserStatusText.setText("Signed in with email " + email);
+            mIsUserSignedIn = true;
         } else {
-            mUserStatusText.setText("Not logged in as");
-            Log.d(TAG, "Not logged in");
-            mIsUserLoggedIn = false;
+            mUserStatusText.setText("Not signed in");
+            Log.i(TAG, "Not signed in");
+            mIsUserSignedIn = false;
+        }
+    }
+
+    private void checkConnectionStatus() {
+
+        // Setup reference to rt database
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        if (database != null) {
+            mConnectionReference = database.getReference(".info");
+            mUsersReference = database.getReference("users");
+            mIsUserConnected = true;
+            if(mIsUserSignedIn == true) {
+                Log.i(TAG, "Update user status on db");
+                mUsersReference.child(mFireBaseUid).child("status").setValue(mIsUserSignedIn);
+
+                // TODO: don't do this every time
+                mConnectionReference.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        boolean connected = dataSnapshot.child("connected").getValue(Boolean.class);
+                        if (connected) {
+                            Log.i(TAG, "User connected");
+                        } else {
+                            Log.i(TAG, "User disconnected");
+                        }
+
+                        OnDisconnect onDisconnect = mUsersReference.child(mFireBaseUid).child("status").onDisconnect();
+                        onDisconnect.setValue(false);
+
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+                mChooseContactButton.setEnabled(true);
+
+            }
+            Log.i(TAG, "User available");
+
+        } else {
+            Log.i(TAG, "User not available");
+            mUsersReference = null;
+            mIsUserConnected = false;
             mChooseContactButton.setEnabled(false);
         }
-        if(mFireBaseUid!=null) {
-            mUsersReference.child(mFireBaseUid).child("status").setValue(mIsUserLoggedIn);
-        }
+
     }
 }
